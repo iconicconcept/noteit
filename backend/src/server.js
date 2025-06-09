4f8f10d9df1a7e85fd503750c5d5a565dacf4d2b
Mubaraq Allamalyekeen <mubaraqadeniyi159@gmail.com>
2025-06-09 12:23:39 +0100

    edit

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,7 +38,7 @@ app.use("/api/notes", noteRoute)
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*",(req, res)=>{
+    app.get("/files{/*path}",(req, res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     });
 };
